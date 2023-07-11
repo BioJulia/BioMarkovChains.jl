c669033da034ae2d6df4863169afeb69005b2a88
Camilo García <ca.garcia2@uniandes.edu.co>
2023-07-11 10:57:19 -0500

    update Make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,17 @@ using BioMarkovChains
 
 # indigo = DocThemeIndigo.install(Configurations)
 
+DocMeta.setdocmeta!(BioMarkovChains, :DocTestSetup, :(using BioMarkovChains); recursive = true)
+
+
 makedocs(;
     modules = [BioMarkovChains],
+    authors = "Camilo García",
+    repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl/blob/{commit}{path}#{line}",
+    sitename = "BioMarkovChains.jl",
     format = Documenter.HTML(
-        prettyurls = !("local" in ARGS),
+        mathengine=MathJax3(),
+        prettyurls = get(ENV, "CI", "false") == "true",
         canonical="https://camilogarciabotero.github.io/BioMarkovChains.jl",
         # assets=String[indigo],
     ),
@@ -16,8 +23,6 @@ makedocs(;
         "Towards Markov Chains" => "biomarkovchains.md",
         "API" => "api.md"
     ],
-    repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl",
-    sitename = "BioMarkovChains.jl",
 )
 
-deploydocs(; repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl")
+deploydocs(; repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl", devbranch = "main")
