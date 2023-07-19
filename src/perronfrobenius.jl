758d8ce6c731e1de7e6e8eedbd9786c5f9bb959c
Camilo García <ca.garcia2@uniandes.edu.co>
2023-07-18 21:26:54 -0500

    add new pf methods

--- src/perronfrobenius.jl
+++ src/perronfrobenius.jl
@@ -6,6 +6,17 @@ function perronfrobenius(sequence::LongNucOrView{4}, n::Int64=1)
     return copy(pf)
 end
 
+function perronfrobenius(bmc::BioMarkovChain)
+   tpm = bmc.tpm
+   pf = transpose(tpm)
+   return copy(pf)
+end
+
+function perronfrobenius(tpm::Matrix{Float64})
+    pf = transpose(tpm)
+    return copy(pf)
+end
+
 function generatedna(pf::Matrix{Float64}, steps::Int64; extended_alphabet::Bool = false)
     newseq = LongDNA{4}()
     # pf = transpose(tpm) # The Perron-Frobenius matrix
