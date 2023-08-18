37b047519e4ea7df9d02b7033e6b6240b6e55d2a
Camilo García <ca.garcia2@uniandes.edu.co>
2023-08-18 15:44:49 -0500

    create a separate file for constants

---
+++ src/constants.jl
@@ -0,0 +1,26 @@
+
+const LongNucOrView{N} = Union{LongSequence{<:NucleicAcidAlphabet{N}},LongSubSeq{<:NucleicAcidAlphabet{N}}}
+const LongAminoAcidOrView = Union{LongSequence{<:AminoAcidAlphabet},LongSubSeq{<:AminoAcidAlphabet}}
+
+const NUCLEICINDEXES = Dict(DNA_A => 1, DNA_C => 2, DNA_G => 3, DNA_T => 4)
+
+const DINUCLEICINDEXES = Dict(
+    LongDNA{4}("AA") => [1, 1],
+    LongDNA{4}("AC") => [1, 2],
+    LongDNA{4}("AG") => [1, 3],
+    LongDNA{4}("AT") => [1, 4],
+    LongDNA{4}("CA") => [2, 1],
+    LongDNA{4}("CC") => [2, 2],
+    LongDNA{4}("CG") => [2, 3],
+    LongDNA{4}("CT") => [2, 4],
+    LongDNA{4}("GA") => [3, 1],
+    LongDNA{4}("GC") => [3, 2],
+    LongDNA{4}("GG") => [3, 3],
+    LongDNA{4}("GT") => [3, 4],
+    LongDNA{4}("TA") => [4, 1],
+    LongDNA{4}("TC") => [4, 2],
+    LongDNA{4}("TG") => [4, 3],
+    LongDNA{4}("TT") => [4, 4],
+)
+
+const AA20 = (AA_A, AA_R, AA_N, AA_D, AA_C, AA_Q, AA_E, AA_G, AA_H, AA_I, AA_L, AA_K, AA_M, AA_F, AA_P, AA_S, AA_T, AA_W, AA_Y, AA_V)
