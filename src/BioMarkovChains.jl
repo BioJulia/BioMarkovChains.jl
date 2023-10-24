b68fa0a920d36440ce4561d198b57dcd3dc78165
Camilo García <ca.garcia2@uniandes.edu.co>
2023-10-23 23:11:57 -0500

    migrate several methods to GeneFinder

--- src/BioMarkovChains.jl
+++ src/BioMarkovChains.jl
@@ -11,11 +11,9 @@ using BioSequences:
     NucleicAcidAlphabet,
     DNA,
     DNAAlphabet,
-    DNA_Gap, DNA_A, DNA_C, DNA_M, DNA_G, DNA_R, DNA_S, DNA_V, DNA_T, DNA_W, DNA_Y, DNA_H, DNA_K, DNA_D, DNA_B, DNA_N,
     
     #RNA
     RNA,
-    RNA_Gap, RNA_A, RNA_C, RNA_M, RNA_G, RNA_R, RNA_S, RNA_V, RNA_U, RNA_W, RNA_Y, RNA_H, RNA_K, RNA_D, RNA_B, RNA_N,
     
     #AminoAcids
     AminoAcid,
@@ -24,32 +22,27 @@ using BioSequences:
     AA_A, AA_R, AA_N, AA_D, AA_C, AA_Q, AA_E, AA_G, AA_H, AA_I, AA_L, AA_K, AA_M, AA_F, AA_P, AA_S, AA_T, AA_W, AA_Y, AA_V, AA_O, AA_U, AA_B, AA_J, AA_Z, AA_X, AA_Term, AA_Gap,
 
     # Other functions
-    SeqOrView,
-    alphabet,
-    @biore_str
+    SeqOrView, NucleicSeqOrView
 
 using PrecompileTools: @setup_workload, @compile_workload
 using TestItems: @testitem
 using StatsAPI: StatsAPI, fit, fit!
-using StatsBase: countmap
 using VectorizedKmers: count_kmers
 
 include("types.jl")
 export BioMarkovChain, BMC
 
 include("utils.jl")
-export transitions, hasprematurestop, randbmc
+export randbmc
 
 include("transitions.jl")
-export initials, transition_count_matrix, transition_probability_matrix, dnaseqprobability, logoddsratio, logoddsratioscore
+export initials, transition_count_matrix, transition_probability_matrix, logoddsratio, logoddsratioscore
 
 include("models.jl")
 export ECOLICDS, ECOLINOCDS
 
 include("perronfrobenius.jl")
 export perronfrobenius, generatedna
-
-include("constants.jl")
 
 include("extended.jl")
 
@@ -61,12 +54,10 @@ include("extended.jl")
     @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
-        transitions(seq)
         transition_count_matrix(seq)
         transition_probability_matrix(seq)
-        dnaseqprobability(seq, ECOLICDS)
-        BioMarkovChain(seq)
         perronfrobenius(seq)
+        BioMarkovChain(seq)
     end
 end
 
--- src/utils.jl
+++ src/utils.jl
@@ -1,62 +1,3 @@
-const LongNucOrView{N} = Union{LongSequence{<:NucleicAcidAlphabet{N}},LongSubSeq{<:NucleicAcidAlphabet{N}}}
-
-const LongAminoAcidOrView = Union{LongSequence{<:AminoAcidAlphabet},LongSubSeq{<:AminoAcidAlphabet}}
-
-"""
-    transitions(sequence::LongSequence)
-
-Compute the transition counts of each pair in a given biological sequence sequence.
-
-# Arguments
-- `sequence::LongSequence{DNAAlphabet{4}}`: a `LongSequence{DNAAlphabet{4}}` object representing the DNA sequence.
-
-# Returns
-A dictionary with keys being `Dict{Tuple{DNA, DNA}, Int64}` objects representing
-the dinucleotides, and values being the number of occurrences of each dinucleotide
-in the sequence.
-
-# Example
-```
-seq = dna"AGCTAGCTAGCT"
-
-dinucleotides(seq)
-Dict{Tuple{DNA, DNA}, Int64} with 4 entries:
-  (DNA_C, DNA_T) => 3
-  (DNA_G, DNA_C) => 3
-  (DNA_T, DNA_A) => 2
-  (DNA_A, DNA_G) => 3
-```
-"""
-function transitions(sequence::SeqOrView{A}) where A
-    b = @view sequence[2:end]
-    return countmap(zip(sequence, b))
-end
-
-"""
-    hasprematurestop(sequence::LongNucOrView{4})::Bool
-
-Determine whether the `sequence` of type `LongSequence{DNAAlphabet{4}}` contains a premature stop codon.
-
-Returns a boolean indicating whether the `sequence` has more than one stop codon.
-"""
-function hasprematurestop(sequence::LongNucOrView{N})::Bool where N
-    
-    stopcodons = [LongDNA{4}("TAA"), LongDNA{4}("TAG"), LongDNA{4}("TGA")]  # Create a set of stop codons
-    
-    length(sequence) % 3 == 0 || error("The sequence is not divisible by 3")
-    
-    occursin(biore"T(AG|AA|GA)"dna, sequence[end-2:end]) || error("There is no stop codon at the end of the sequence")
-
-    @inbounds for i in 1:3:length(sequence) - 4
-        codon = sequence[i:i+2]
-        if codon in stopcodons
-            return true
-        end
-    end
-
-    return false
-end
-
 function _int_to_dna(index::Int64; extended_alphabet::Bool = false)
     A = extended_alphabet ? [DNA_Gap, DNA_A, DNA_C, DNA_M, DNA_G, DNA_R, DNA_S, DNA_V, DNA_T, DNA_W, DNA_Y, DNA_H, DNA_K, DNA_D, DNA_B, DNA_N] : [DNA_A, DNA_C, DNA_G, DNA_T]
     return LongSequence{DNAAlphabet{4}}([A[index]])
@@ -67,15 +8,15 @@ function _dna_to_int(nucleotide::DNA; extended_alphabet::Bool = false)
     return findfirst(nucleotide, LongSequence{DNAAlphabet{4}}(A))
 end
 
-function randbmc(A::DataType, n::Int64=1)
-    if A == DNA || A == RNA
+function randbmc(statespace::DataType, n::Int64=1)
+    if statespace == DNA || statespace == RNA
         tpm = rand(4, 4)
         row_sums = sum(tpm, dims=2)
         inits = rand(4)
         init_sum = sum(inits)
         @views tpm ./= row_sums
         @views inits ./= init_sum
-    elseif A == AminoAcid
+    elseif statespace == AminoAcid
         tpm = rand(20, 20)
         row_sums = sum(tpm, dims=2)
         inits = rand(20)
@@ -86,5 +27,5 @@ function randbmc(A::DataType, n::Int64=1)
         error("Alphabet must be of the DNA, RNA or AminoAcid DataType")
     end
 
-    return BMC(tpm, inits, n)
+    return BMC(statespace, tpm, inits, n)
 end
