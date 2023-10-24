562bde4e04d61c9b6d577804fe57205d2d8ac9c7
Camilo García <ca.garcia2@uniandes.edu.co>
2023-10-23 23:11:36 -0500

    add a new possible extension

--- ext/MarkovChainHammerExt.jl
+++ ext/MarkovChainHammerExt.jl
@@ -1,6 +1,12 @@
-# module MarkovChainHammerExt
+module MarkovChainHammerExt
 
-# using BioMarkovChains
-# using MarkovChainHammer.Trajectory: generate
+using BioMarkovChains
+using MarkovChainHammer: generate
 
-# end
+function MarkovChainHammer.generate(pf::Matrix{Float64}, steps::Int64)
+    return generate(pf, steps)
+end
+
+export generate
+
+end
