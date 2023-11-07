ac57249782d48c3d40602a915abe02443ec6e1d8
Camilo García <ca.garcia2@uniandes.edu.co>
2023-11-06 19:15:56 -0500

    Remove unused constants and fix end of file in extended.jl

--- src/constants.jl
+++
@@ -1,3 +0,0 @@
-# const AA20 = (AA_A, AA_R, AA_N, AA_D, AA_C, AA_Q, AA_E, AA_G, AA_H, AA_I, AA_L, AA_K, AA_M, AA_F, AA_P, AA_S, AA_T, AA_W, AA_Y, AA_V)
-
-# mtx =  [(ACGT[i], ACGT[j]) for i in 1:4, j in 1:4]
