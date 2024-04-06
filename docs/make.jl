578d8de46c730a4a56fe59780fd3451b9f1f2f7a
Camilo García <ca.garcia2@uniandes.edu.co>
2024-04-06 16:55:38 -0500

    Add DocumenterCitations package to Project.toml and update make.jl for documentation generation

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,28 @@
 using Documenter
 using BioMarkovChains
-# using DocThemeIndigo
-
-# indigo = DocThemeIndigo.install(Configurations)
 
 DocMeta.setdocmeta!(BioMarkovChains, :DocTestSetup, :(using BioMarkovChains); recursive = true)
 
+fmt = Documenter.HTML(
+    mathengine=MathJax3(),
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical="https://camilogarciabotero.github.io/BioMarkovChains.jl",
+    assets=String[],
+)
+
+pgs = [
+    "Home" => "index.md",
+    "Towards Markov Chains" => "biomarkovchains.md",
+    "API" => "api.md"
+]
 
 makedocs(;
     modules = [BioMarkovChains],
     authors = "Camilo García",
     repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl/blob/{commit}{path}#{line}",
     sitename = "BioMarkovChains.jl",
-    format = Documenter.HTML(
-        mathengine=MathJax3(),
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical="https://camilogarciabotero.github.io/BioMarkovChains.jl",
-        # assets=String[indigo],
-    ),
-    pages = [
-        "Home" => "index.md",
-        "Towards Markov Chains" => "biomarkovchains.md",
-        "API" => "api.md"
-    ],
+    format = fmt,
+    pages = pgs,
 )
 
 deploydocs(; repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl", devbranch = "main")
