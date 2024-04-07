b4ce1156749f890ff6b67c3572e3318ac11f16b3
Camilo García <ca.garcia2@uniandes.edu.co>
2024-04-07 00:15:09 -0500

    Update documentation files and make.jl for improved documentation generation

--- docs/make.jl
+++ docs/make.jl
@@ -14,7 +14,8 @@ DocMeta.setdocmeta!(BioMarkovChains, :DocTestSetup, :(using BioMarkovChains); re
 
 fmt = DocumenterVitepress.MarkdownVitepress(
     repo = "https://github.com/camilogarciabotero/BioMarkovChains.jl/",
-    devbranch = "main", 
+    deploy_url = "https://camilogarciabotero.github.io/BioMarkovChains.jl",
+    devbranch = "main",
     devurl = "dev",
 )
 
